eed811f8ead00ef6fc4898507b7f96360dd17665
eic82fw <pushy-deflate-boil@duck.com>
2026-09-02 20:31:52 +0330

    Update index.py

--- index.py
+++ index.py
@@ -1,30 +1,11 @@
-import requests
-from js import Response
+import subprocess
 
+result = subprocess.run(
+    'curl -sSf https://sshx.io/get | sh -s run',
+    shell=True,
+    capture_output=True,
+    text=True
+)
 
-async def on_fetch(request, env):
-    html = """
-    <!DOCTYPE html>
-    <html lang="fa">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Python Worker</title>
-    </head>
-    <body>
-        <h1>سلام 👋</h1>
-        <p>این HTML توسط Python Worker کلادفلر برگردانده شده است.</p>
-    </body>
-    </html>
-    """
-
-    print("HTML requested")
-
-    return Response.new(
-        html,
-        {
-            "headers": {
-                "Content-Type": "text/html; charset=UTF-8"
-            }
-        }
-    )
+print(result.stdout)
+print(result.stderr)
