1b719104e9ce4059fdc331919e956027d7334479
eic82fw <pushy-deflate-boil@duck.com>
2026-09-02 17:35:40 +0330

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,30 @@
-from workers import WorkerEntrypoint, Response
+import requests
+from js import Response
 
 
-class Default(WorkerEntrypoint):
-    async def fetch(self, request):
-        return Response(
-            "Hello from Python + Cloudflare 🚀",
-            headers={
-                "content-type": "text/plain; charset=utf-8"
+async def on_fetch(request, env):
+    html = """
+    <!DOCTYPE html>
+    <html lang="fa">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Python Worker</title>
+    </head>
+    <body>
+        <h1>سلام 👋</h1>
+        <p>این HTML توسط Python Worker کلادفلر برگردانده شده است.</p>
+    </body>
+    </html>
+    """
+
+    print("HTML requested")
+
+    return Response.new(
+        html,
+        {
+            "headers": {
+                "Content-Type": "text/html; charset=UTF-8"
             }
-        )
+        }
+    )
